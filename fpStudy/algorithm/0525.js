8a7c3a54116956b2e4ec8119f572b77cfe6264aa
chany <lyhy0310@gmail.com>
2019-05-25 23:34:00 +0900

    update: daily til

---
+++ fpStudy/algorithm/0525.js
@@ -0,0 +1,13 @@
+function solution(array, commands) {
+  console.log(commands)
+  
+  return commands.map(command => {
+    console.log(array.slice(command[0]-1, command[1]))
+    return array.slice(command[0]-1, command[1]).sort((a,b)=>a-b)[command[2]-1]
+  })
+}
+
+console.log(
+  solution([1, 5, 2, 6, 3, 7, 4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
+
+	
